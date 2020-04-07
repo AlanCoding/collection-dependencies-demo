5f905d72396a541e049a6c9d15ed2befde21f976
AlanCoding <arominge@redhat.com>
2020-04-07 14:35:25 -0400

    remove comment

--- cloner/get_sources.py
+++ cloner/get_sources.py
@@ -69,14 +69,3 @@ with open(dest_path, 'w') as f:
 
 print('--------')
 print('\n'.join(clones))
-
-# next_link = 'https://galaxy.ansible.com/api/v2/collections/'
-# 
-# while next_link:
-#     r = requests.get(next_link)
-#     assert r.status_code == 200
-#     data = r.json()
-#     for entry in data['results']:
-#         print('{}.{}'.format(entry['namespace']['name'], entry['name']))
-#     next_link = 'https://galaxy.ansible.com{}'.format(data['next'])
-
